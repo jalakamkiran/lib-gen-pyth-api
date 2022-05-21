da665b9da59c50f18ccc436a3fc1715585c527c2
Kiran Sai <39314292+jalakamkiran@users.noreply.github.com>
2022-05-21 14:51:04 +0530

    Added image url for cover photos

--- exscript.py
+++ exscript.py
@@ -4,17 +4,21 @@ from flask import request
 from flask import Flask,json
 from libgen_api import LibgenSearch
 from pprint import pprint
+from bs4 import BeautifulSoup
+import requests
 
 app = Flask(__name__)
 
-
-
-
 def searchBook(title):
     s = LibgenSearch()
-    print(title)
+    finalSearchResult = []
     results = s.search_title(title)
-    pprint(results)
+    result = results
+    for i in result:
+        mirror1 = i["Mirror_1"]
+        imgurl = extractCoverImageOfBook(mirror1)
+        i["coverImage"] = imgurl["imageUrl"]
+        finalSearchResult.append(i)
     return json.dumps(results)
 
 def searchAuthor(author):
@@ -29,6 +33,13 @@ def downloadBook(itemToDownload):
     print(download_links)
     return download_links
 
+def extractCoverImageOfBook(url):
+    page = requests.get(url=url)
+    soup = BeautifulSoup(page.text, "html.parser")
+    links = soup.find("img")
+    print(links["src"]) 
+    return json.dumps({"imageUrl": "http://library.lol/" + links["src"]})
+
 @app.route('/searchBooks',methods=['GET'])
 def get_books():
     searchText = request.args.get('bookTitle')
@@ -42,7 +53,16 @@ def download_book():
         return downloadBook(itemToDownload) 
     else:
         return 'Content-Type not supported!'    
-     
+
+
+@app.route('/getBookCover',methods=['POST'])
+def getBookCover(): 
+    content_type = request.headers.get('Content-Type')
+    if (content_type == 'application/json'):
+        bookUrl = request.json
+        return extractCoverImageOfBook(bookUrl["url"]) 
+    else:
+        return 'Content-Type not supported!'      
   
 # main driver function
 if __name__ == '__main__':
